b9d83068271c859e6b7b5a9db28a3f7279e126a0
klu2300032719 <2300032719@kluniversity.in>
2025-09-24 12:08:39 +0530

    Initial commit with backend, frontend, and docker setup

---
+++ client/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { AppContextProvider } from './context/AppContext.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter basename={import.meta.env.VITE_BASE || '/'}>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
