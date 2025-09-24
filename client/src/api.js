5505325859abcbae1f28ddc49b7520b705be5698
klu2300032719 <2300032719@kluniversity.in>
2025-09-24 15:37:46 +0530

    Add CI/CD workflows

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:2025/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:2025",
 });
 
 export default api;
